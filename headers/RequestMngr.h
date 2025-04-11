efcd787f99a3585206decb7c2f0f493121519d01
LAisdy <keran5509@gmail.com>
2025-04-11 17:00:20 +0300

    wip: now use libsodium to encrypt msg, implement some of send/get funcs

--- headers/RequestMngr.h
+++ headers/RequestMngr.h
@@ -5,18 +5,26 @@
 #include <optional>
 #include <stdexcept>
 #include <windows.h>
+#include <wincrypt.h>
 #include <rpc.h>
 #include <string>
+#include <vector>
+#include <fstream>
 #include <filesystem>
 #include <tlhelp32.h>
 #include <iostream>
+
+#define SODIUM_STATIC
+#include <sodium.h>
+
+
 //nlohmann-json
 #include <json.hpp>
+
 //openssl
 #include <evp.h>
 #include <rand.h>
 #include <bio.h>
-
 //curl
 #include <curl.h>
 
@@ -26,25 +34,34 @@ public:
 	RequestMngr();
 	~RequestMngr();
 
-	void start_async_send(); 
-	bool is_loading() const;
+	void start_async_send(DWORD pid);
 	void get_process_dlls(DWORD pid);
 	std::vector<std::wstring> dlls;
+	int init_key();
+	void show_warning(std::wstring msg, std::wstring name);
 
-	
-	
 private:
 
 	mutable std::mutex dll_mutex;
 	std::vector<std::wstring> dll_list;
 	std::string json_dlls;
+	std::string last_rid;
+	std::vector<BYTE> key;
 	//std::atomic<bool> is_loading{ false };
 	std::thread worker_thread_;
 
 	bool ends_with(const std::wstring& str, const std::wstring& endStr);
 	void extract_filename(std::wstring& path);
-	void gen_dlls_string() { nlohmann::json j = dlls; json_dlls = j.dump(); };
+
 	std::string gen_rid();
 	std::string encode_string();
+
+	std::vector<BYTE> hide_key_DPAPI(const std::vector<BYTE>& key);
+	std::vector<BYTE> reveal_key_DPAPI(const std::vector<BYTE>& protectedData);
+	bool save_protected_key(const std::string& filePath, const std::vector<BYTE>& protectedKey);
+	bool load_protected_key(const std::string& filePath, std::vector<BYTE>& protectedKey);
+	std::vector<BYTE> gen_key();
+	std::vector<unsigned char> encrypt(const std::string& message, const std::vector<unsigned char>& key);
+	std::string decrypt(const std::vector<unsigned char>& encrypted, const std::vector<unsigned char>& key);
 };
 
--- src/RequestMngr.cpp
+++ src/RequestMngr.cpp
@@ -13,8 +13,53 @@ RequestMngr::~RequestMngr()
     }
 }
 
-void RequestMngr::start_async_send()
+void RequestMngr::start_async_send(DWORD pid)
 {
+    /*
+    * get dll loaded by proc: 
+    * get_process_dlls(pid) - this will store the dll seq as a string into json_dlls
+    * 
+    * encrypt(json_dlls, key)
+    * 
+    * create json body for msg: {"cmd:1", "rid:<some_random_val>","data: <encrypted_data>"}
+    * send using curl 
+    * TODO: implement curl functions 
+    */
+}
+
+void RequestMngr::show_warning(std::wstring msg, std::wstring name )
+{
+    MessageBox(
+        NULL,                           
+        msg.c_str(),
+        name.c_str(),
+        MB_OK | MB_ICONWARNING          
+    );
+}
+
+int RequestMngr::init_key()
+{
+    std::string keyFile = "protected_key.bin";
+    std::vector<BYTE> protectedKey;
+    
+    if (!load_protected_key(keyFile, protectedKey)) 
+    {
+        key = gen_key();
+        protectedKey = hide_key_DPAPI(key);
+        if (!save_protected_key(keyFile, protectedKey))
+        {
+            show_warning(L"Failed to save key", L"Warning");
+            return 1;
+        }
+    }
+    else 
+    {
+        key = reveal_key_DPAPI(protectedKey);
+        if (key.size() != crypto_secretbox_KEYBYTES) {
+            show_warning(L"Incorrect key length after unpacking", L"Warning");
+            return 1;
+        }
+    }
 }
 
 std::string RequestMngr::gen_rid()
@@ -34,12 +79,6 @@ std::string RequestMngr::gen_rid()
     RpcStringFreeA(&str);
 
     return rid;
-}
-
-std::string RequestMngr::encode_string()
-{
-    
-    return {};
 }
 
 void RequestMngr::get_process_dlls(DWORD pid)
@@ -68,6 +107,10 @@ void RequestMngr::get_process_dlls(DWORD pid)
             }
         } while (Module32NextW(hSnapshot, &me));
     }
+
+    nlohmann::json j = dlls;
+    json_dlls = j.dump();
+
     CloseHandle(hSnapshot);
 }
 
@@ -77,10 +120,109 @@ void RequestMngr::extract_filename(std::wstring& path)
     path = fs_path.filename().wstring();
 }
 
-
 bool RequestMngr::ends_with(const std::wstring& str, const std::wstring& endStr)
 {
     if (str.size() < endStr.size())
         return false;
     return str.compare(str.size() - endStr.size(), endStr.size(), endStr) == 0;
+}
+
+
+std::vector<BYTE> RequestMngr::hide_key_DPAPI(const std::vector<BYTE>& key)
+{
+    DATA_BLOB inBlob, outBlob;
+    inBlob.pbData = const_cast<BYTE*>(key.data());
+    inBlob.cbData = static_cast<DWORD>(key.size());
+
+    if (!CryptProtectData(&inBlob, L"Libsodium Key", nullptr, nullptr, nullptr, 0, &outBlob))
+        throw std::runtime_error("CryptProtectData failed");
+
+    std::vector<BYTE> protectedData(outBlob.pbData, outBlob.pbData + outBlob.cbData);
+    LocalFree(outBlob.pbData);
+    return protectedData;
+}
+
+std::vector<BYTE> RequestMngr::reveal_key_DPAPI(const std::vector<BYTE>& protectedData)
+{
+    DATA_BLOB inBlob, outBlob;
+    inBlob.pbData = const_cast<BYTE*>(protectedData.data());
+    inBlob.cbData = static_cast<DWORD>(protectedData.size());
+
+    if (!CryptUnprotectData(&inBlob, nullptr, nullptr, nullptr, nullptr, 0, &outBlob))
+        throw std::runtime_error("CryptUnprotectData failed");
+
+    std::vector<BYTE> key(outBlob.pbData, outBlob.pbData + outBlob.cbData);
+    LocalFree(outBlob.pbData);
+    return key;
+}
+
+bool RequestMngr::save_protected_key(const std::string& filePath, const std::vector<BYTE>& protectedKey)
+{
+    std::ofstream ofs(filePath, std::ios::binary);
+    if (!ofs)
+    {
+        return false;
+    }
+    ofs.write(reinterpret_cast<const char*>(protectedKey.data()), protectedKey.size());
+    return ofs.good();
+}
+
+bool RequestMngr::load_protected_key(const std::string& filePath, std::vector<BYTE>& protectedKey)
+{
+    std::ifstream ifs(filePath, std::ios::binary);
+    if (!ifs)
+        return false;
+    protectedKey = std::vector<BYTE>((std::istreambuf_iterator<char>(ifs)),
+        std::istreambuf_iterator<char>());
+    return !protectedKey.empty();
+}
+
+std::vector<BYTE> RequestMngr::gen_key()
+{
+    std::vector<BYTE> key(crypto_secretbox_KEYBYTES);
+    randombytes_buf(key.data(), key.size());
+    return key;
+}
+
+std::vector<unsigned char> RequestMngr::encrypt(const std::string& message, const std::vector<unsigned char>& key)
+{
+    if (key.size() != crypto_secretbox_KEYBYTES)
+        throw std::runtime_error("Incorrect key length");
+
+    std::vector<unsigned char> nonce(crypto_secretbox_NONCEBYTES);
+    randombytes_buf(nonce.data(), nonce.size());
+
+    std::vector<unsigned char> ciphertext(crypto_secretbox_MACBYTES + message.size());
+    if (crypto_secretbox_easy(ciphertext.data(),
+        reinterpret_cast<const unsigned char*>(message.data()),
+        message.size(),
+        nonce.data(),
+        key.data()) != 0)
+    {
+        throw std::runtime_error("crypto_secretbox_easy failed");
+    }
+
+    nonce.insert(nonce.end(), ciphertext.begin(), ciphertext.end());
+    return nonce;
+}
+
+std::string RequestMngr::decrypt(const std::vector<unsigned char>& encrypted, const std::vector<unsigned char>& key)
+{
+    if (encrypted.size() < crypto_secretbox_NONCEBYTES + crypto_secretbox_MACBYTES)
+        throw std::runtime_error("Incorrect length of encrypted data");
+
+    std::vector<unsigned char> nonce(encrypted.begin(), encrypted.begin() + crypto_secretbox_NONCEBYTES);
+    
+    std::vector<unsigned char> ciphertext(encrypted.begin() + crypto_secretbox_NONCEBYTES, encrypted.end());
+
+    std::vector<unsigned char> decrypted(ciphertext.size() - crypto_secretbox_MACBYTES);
+    if (crypto_secretbox_open_easy(decrypted.data(),
+        ciphertext.data(),
+        ciphertext.size(),
+        nonce.data(),
+        key.data()) != 0)
+    {
+        throw std::runtime_error("crypto_secretbox_open_easy failed");
+    }
+    return std::string(decrypted.begin(), decrypted.end());
 }
